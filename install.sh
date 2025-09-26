99fd3fc884875a74f684f63e7c78fd38f498cc8c
DrMrMcMisterEr <kornelius.lind@gmail.com>
2025-09-26 21:59:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
-echo "Paul's absolut Hypergeiler selfinstaller"
-echo "#1 Jetzt mach ma mal wieder a weng sam und installieren alles in und um Flatpaks"
-#insert flatpak installation commands here
-echo "schon mal das geschafft."
-echo "#2 Jetzt installieren wir alles in und um APT. Nutze übrigens APT nur für systempakete, alle Apps solltest du über Flatpaks installieren!"
-#insert APT installation command
+#Pauls absolut Hypergeiler selfinstaller
+#1 Jetzt mach ma mal wieder a weng sam und installieren alles in und um Flatpaks
+sudo flatpak install podmandesktop pods protonvpn torbrowser-launcher vesktop com.valvesoftware.steam
+#schon mal das geschafft.
+#2 Jetzt installieren wir alles in und um APT. Nutze übrigens APT nur für systempakete, alle Apps solltest du über Flatpaks installieren!"
+sudo apt update 
+sudo apt install btop neofetch fastfetch podman distrobox 
