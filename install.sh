6d2eb53aec2095a97a7798d8e75e925d2182000e
DrMrMcMisterEr <kornelius.lind@gmail.com>
2025-09-26 21:10:25 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+echo "Paul's absolut Hypergeiler selfinstaller"
+echo "#1 Jetzt mach ma mal wieder a weng sam und installieren alles in und um Flatpaks"
+#insert flatpak installation commands here
+echo "schon mal das geschafft."
+echo "#2 Jetzt installieren wir alles in und um APT. Nutze übrigens APT nur für systempakete, alle Apps solltest du über Flatpaks installieren!"
+#insert APT installation command
